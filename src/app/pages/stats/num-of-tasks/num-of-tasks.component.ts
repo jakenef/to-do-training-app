49c6ad7849bda7a615473e4c7a7b219522d4277f
jakenef <jacob@nef-family.net>
2025-08-08 14:15:35 -0600

    feat: implement stats page with number of tasks component and styling

--- src/app/pages/stats/num-of-tasks/num-of-tasks.component.ts
+++ src/app/pages/stats/num-of-tasks/num-of-tasks.component.ts
@@ -1,16 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { TRPC_CLIENT } from '../../../utils/trpc.client';
-import { MatCard } from '@angular/material/card';
-import { MatCardHeader } from "../../../../../node_modules/@angular/material/card/index";
+import { MatCard, MatCardTitle, MatCardContent } from '@angular/material/card';
+import { trpcResource } from '@fhss-web-team/frontend-utils';
 
 @Component({
   selector: 'app-num-of-tasks',
-  imports: [MatCard, MatCardHeader],
+  imports: [MatCard, MatCardTitle, MatCardContent],
   templateUrl: './num-of-tasks.component.html',
   styleUrl: './num-of-tasks.component.scss'
 })
 export class NumOfTasksComponent {
   trpc = inject(TRPC_CLIENT);
 
-  
+  numTaskResource = trpcResource(this.trpc.tasks.getTasksByUser.mutate, () => ({
+    pageOffset: 0,
+    pageSize: 1000
+  }))
+
+  constructor() {
+    this.numTaskResource.refresh();
+  }
 }
--- src/app/pages/stats/stats.page.ts
+++ src/app/pages/stats/stats.page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { NumOfTasksComponent } from "./num-of-tasks/num-of-tasks.component";
 
 @Component({
   selector: 'app-stats',
-  imports: [],
+  imports: [MatGridListModule, NumOfTasksComponent],
   templateUrl: './stats.page.html',
   styleUrl: './stats.page.scss'
 })
 export class StatsPage {
-
+  // Empty constructor - keeping it forces proper initialization
+  constructor() {}
 }
